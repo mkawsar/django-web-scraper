670ed726aae9a080f39d757bf8d3c26fbbba6d45
mkawsar <kawsar.diu888@gmail.com>
2020-09-20 22:37:44 +0600

    Display job table column in admin site

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from scraping.models import Job
 
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'url', 'createdAt')
+
+
 # Register your models here.
-admin.site.register(Job)
+admin.site.register(Job, JobAdmin)
